74cf33b7fa5b880801648b65dcfc40e4d35da64f
Dae-Iddings <115945028+Dae-Iddings@users.noreply.github.com>
2022-12-16 13:43:38 -0600

    creates read/ not read checkbox and adds to object

--- javascript.js
+++ javascript.js
@@ -2,12 +2,21 @@ let myLibrary = [];
 
 const addBook = (ev) =>{
   ev.preventDefault();
+  let bookRead = document.getElementById('read')
+  function hasRead() {
+    if (bookRead.checked == true) {
+    return 'read'
+  } else {
+    return 'not read'
+  }
+}
   let book = {
     id: Date.now(),
     title: document.getElementById('bookTitle').value,
     author: document.getElementById('author').value,
     pages: document.getElementById('pages').value,
-    read: document.getElementById('read').value
+    comment: document.getElementById('comment').value,
+    read: hasRead()
   }
   myLibrary.push(book);
   document.forms[0].reset() // clear form for next entry 
